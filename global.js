6f264dcab762471bd0575e28944031337ba632e0
blooke21 <141855101+blooke21@users.noreply.github.com>
2024-01-19 18:19:13 -0600

    Add files via upload

--- global.js
+++ global.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
   /* TODO
     replace all instances of 2px with a css border variable
     replace all instance where inside-boxes width and height are variables that are related to box-container
@@ -9,18 +10,89 @@ $(document).ready(function () {
     return Math.floor(Math.random() * num);
   }
 
+  //will add cookie check to see if it is users first time
+  //if users first time play spotlight animation and delay all other start-up animations by 1s
+  var firstTime = true;
+  var insideBoxDelay = 1;
+  //used to control bobbles delay timing
+  var boxGrowTime = .25;
+
+  var windowWidth = $(window).width();
+  console.log("Window Width: " + windowWidth);
+
+
+  if (firstTime) {
+    //adds extra delay for spotlight animation
+    insideBoxDelay += 4.5;
+    boxGrowTime += 4.5;
+    //moves spotlight around
+    //two different animation depending on screen width
+    if (windowWidth >= 500) {
+      $(".spotlight").animate({ "background-position-x": "-20em", "background-position-y": "5em" }, 800)
+        .promise().done(
+          $(".spotlight").delay(200).animate({ "background-position-x": "10em", "background-position-y": "5em" }, 500))
+        .promise().done(
+          $(".spotlight").delay(200).animate({ "background-position-x": "-20em", "background-position-y": "-5em" }, 300)
+        )
+        .promise().done(
+          $(".spotlight").delay(200).animate({ "background-position-x": "30em", "background-position-y": "-9em" }, 800)
+        )
+        .promise().done(
+          $(".spotlight").delay(200).animate({ "background-position-x": "-10em", "background-position-y": "-9em" }, 800)
+        )
+        .promise().done(
+          $(".spotlight").animate({ "background-position-x": "0em", "background-position-y": "-10em" }, 200)
+        )
+        .promise().done(
+          $(".spotlight").delay(200).animate({ "opacity": "0" }, 500)
+        )
+    } else {
+      $(".spotlight").animate({ "background-position-x": "3em", "background-position-y": "10em" }, 800)
+        .promise().done(
+          $(".spotlight").delay(200).animate({ "background-position-x": "-3em", "background-position-y": "-12em" }, 500))
+        .promise().done(
+          $(".spotlight").delay(200).animate({ "background-position-x": "3em", "background-position-y": "1em" }, 300)
+        )
+        .promise().done(
+          $(".spotlight").delay(200).animate({ "background-position-x": "7em", "background-position-y": "-7em" }, 800)
+        )
+        .promise().done(
+          $(".spotlight").delay(200).animate({ "background-position-x": "-8em", "background-position-y": "-10em" }, 800)
+        )
+        .promise().done(
+          $(".spotlight").animate({ "background-position-x": "0em", "background-position-y": "-10em" }, 200)
+        )
+        .promise().done(
+          $(".spotlight").delay(200).animate({ "opacity": "0" }, 500)
+        )
+    }
+
+  } else {
+    //if it isn't the users first time on the page do not play spotlight animation
+    $(".spotlight").css({ "display": "none" })
+  }
+
+
+  //sets the animations for inside box and bobbles, this is done in JS so a delay can be added if the spotlight animation plays
+  $(".inside-box").css({ "animation": "removeInsideBox 1s " + insideBoxDelay + "s ease-in-out forwards" })
+    .promise().done(
+      //since spotlight has a z index greater than ever other element this shrinks it so the future hover functions work
+      $(".spotlight").delay(200).animate({ "height": "0" }, 10)
+    )
+  $("#bobbleOne").css({ "animation": "growBox 1s " + boxGrowTime + "s ease-in-out forwards" })
+  $("#bobbleTwo").css({ "animation": "growBox 1s " + (boxGrowTime + .35) + "s ease-in-out forwards" })
+  $("#bobbleThree").css({ "animation": "growBox 1s " + (boxGrowTime + .15) + "s ease-in-out forwards" })
+  $("#bobbleOne .inside-bobble").css({ "animation": "text-slide 1s " + (boxGrowTime + 1) + "s ease-in-out forwards" })
+  $("#bobbleTwo .inside-bobble").css({ "animation": "text-slide 1s " + (boxGrowTime + 1.35) + "s ease-in-out forwards" })
+  $("#bobbleThree .inside-bobble").css({ "animation": "text-slide 1s " + (boxGrowTime + 1.15) + "s ease-in-out forwards" })
+
   $(".box-container").each(function () {
     //loads inside-box to random corner
-    // var ranLocation = randomNumber(4);
-    var ranLocation = 0;
+    var ranLocation = randomNumber(4);
     console.log("randomLocation: " + ranLocation);
     //grabs width & height of box-container and subtracts inside-box & border so hover animate sets inside-box to correct location
     var width = $(this).width() - $(this).find(".inside-box").width() + 2;
     var height = $(this).height() - $(this).find(".inside-box").height() + 2;
-    //grabs width & height but does not subtract the width of inside-box and the border. Used to ensure inside-box moves to the far end of the box-container
-    var totalWidth = $(this).width();
-    var totalHeight = $(this).height();
-
     console.log("width: " + width + "\nheight: " + height);
 
     // 0 = box will be on top-left
@@ -69,7 +141,7 @@ $(document).ready(function () {
           case 1: //moves inside-box top to bottom along the right side of box-container
             //stretches height and moves inside-box over so the only width showing is the same as the border width
             $(this).find(".inside-box").css({
-              "margin-left": totalWidth,
+              "margin-left": $(this).width(),
               height: "100px",
               width: "2px",
             });
@@ -79,7 +151,7 @@ $(document).ready(function () {
               .animate({ marginTop: height }, 800)
               //returns inside-box to normal size
               .promise()
-              .done($(this).find(".inside-box").animate({ width: "50px" }, 100))
+              .done($(this).find(".inside-box").animate({ width: "100px" }, 100))
               .promise()
               .done(
                 $(this)
@@ -94,7 +166,7 @@ $(document).ready(function () {
             //stretches inside-box to double is width and shrinks is height to the height of the border for animation
             $(this)
               .find(".inside-box")
-              .css({ width: "100px", height: "2px", marginTop: totalHeight });
+              .css({ width: "100px", height: "2px", marginTop: $(this).height() });
 
             $(this)
               .find(".inside-box")
@@ -132,7 +204,7 @@ $(document).ready(function () {
             break;
         }
       },
-      function () {}
+      function () { }
     );
   });
 });
